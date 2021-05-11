ea242856c97b970a627b3055f21b12e99dec260b
Globox1997 <mitschke11@web.de>
2021-05-11 21:59:47 +0200

    new allow tag

--- src/main/java/net/environmentz/init/TagInit.java
+++ src/main/java/net/environmentz/init/TagInit.java
@@ -10,6 +10,7 @@ public class TagInit {
   // Item
   public static final Tag<Item> WARM_ARMOR = TagRegistry.item(new Identifier("environmentz", "warm_armor"));
   public static final Tag<Item> INSOLATING_ITEM = TagRegistry.item(new Identifier("environmentz", "insolating_item"));
+  public static final Tag<Item> ALLOWED_ARMOR = TagRegistry.item(new Identifier("environmentz", "allowed_armor"));
   // Block
   public static final Tag<Block> WARMING_BLOCKS = TagRegistry.block(new Identifier("environmentz", "warming_blocks"));
 
