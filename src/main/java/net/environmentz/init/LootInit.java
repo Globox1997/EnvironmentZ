83a825aeaa2551f3357a642fb9374fbfdae3bad5
Globox1997 <mitschke11@web.de>
2022-06-13 15:12:47 +0200

    updated deps

--- src/main/java/net/environmentz/init/LootInit.java
+++ src/main/java/net/environmentz/init/LootInit.java
@@ -1,21 +1,21 @@
 package net.environmentz.init;
 
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 
 public class LootInit {
 
     public static void init() {
-        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
             if ("minecraft:entities/wolf".equals(id.toString())) {
-                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder().rolls(BinomialLootNumberProvider.create(3, 0.6F)).with(ItemEntry.builder(ItemInit.WOLF_PELT_ITEM));
-                supplier.pool(poolBuilder);
+                LootPool pool = LootPool.builder().with(ItemEntry.builder(ItemInit.WOLF_PELT_ITEM).build()).rolls(BinomialLootNumberProvider.create(3, 0.6F)).build();
+                supplier.pool(pool);
             }
             if ("minecraft:entities/polar_bear".equals(id.toString())) {
-                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder().rolls(BinomialLootNumberProvider.create(2, 0.5F)).with(ItemEntry.builder(ItemInit.POLAR_BEAR_FUR_ITEM));
-                supplier.pool(poolBuilder);
+                LootPool pool = LootPool.builder().with(ItemEntry.builder(ItemInit.WOLF_PELT_ITEM).build()).rolls(BinomialLootNumberProvider.create(2, 0.5F)).build();
+                supplier.pool(pool);
             }
         });
     }
