329fad7fe296024547403d35df86999f5f0fa70f
Globox1997 <mitschke11@web.de>
2023-05-02 16:48:42 +0200

    increased durability

--- src/main/java/net/environmentz/init/ItemInit.java
+++ src/main/java/net/environmentz/init/ItemInit.java
@@ -12,10 +12,10 @@ import net.minecraft.util.registry.Registry;
 
 public class ItemInit {
     // Item
-    public static final HeatingStones HEATING_STONES_ITEM = new HeatingStones(new Item.Settings().group(ItemGroup.MISC).maxDamage(60));
+    public static final HeatingStones HEATING_STONES_ITEM = new HeatingStones(new Item.Settings().group(ItemGroup.MISC).maxDamage(120));
     public static final WolfPeltItem WOLF_PELT_ITEM = new WolfPeltItem(new Item.Settings().group(ItemGroup.MISC));
     public static final PolarBearFurItem POLAR_BEAR_FUR_ITEM = new PolarBearFurItem(new Item.Settings().group(ItemGroup.MISC));
-    public static final IcePack ICE_PACK_ITEM = new IcePack(new Item.Settings().group(ItemGroup.MISC).maxDamage(60));
+    public static final IcePack ICE_PACK_ITEM = new IcePack(new Item.Settings().group(ItemGroup.MISC).maxDamage(120));
     // Armor
     public static final ArmorMaterial WOLF_ARMOR_MATERIAL = new WolfArmorMaterial();
     public static final Item WOLF_HELMET = new WolfArmor(WOLF_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT));
